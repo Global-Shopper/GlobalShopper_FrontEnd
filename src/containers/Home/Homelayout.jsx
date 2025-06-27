d54d63d69ece2d97cd88ce547fa99f45d61551ab
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2025-06-27 19:49:05 +0700

    feat: homelayout & header UI

--- src/containers/Home/Homelayout.jsx
+++ src/containers/Home/Homelayout.jsx
@@ -1,9 +1,13 @@
+import Header from '@/components/Header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const Homelayout = () => {
   return (
-    <Outlet />
+    <>
+      <Header />
+      <Outlet />
+    </>
   )
 }
 
