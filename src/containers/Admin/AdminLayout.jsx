ba62c41cb66eb207132d57316a136de7f90b45f8
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-07-22 22:55:05 +0700

    refactor: update admin sidebar table

--- src/containers/Admin/AdminLayout.jsx
+++ src/containers/Admin/AdminLayout.jsx
@@ -1,14 +1,14 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AdminSidebar } from "@/components/AdminSidebar"
-import { Outlet } from "react-router-dom"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AdminSidebar } from "@/components/AdminSidebar";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  return (
-    <SidebarProvider>
-      <AdminSidebar />
-      <main>
-        <Outlet />
-      </main>
-    </SidebarProvider>
-  )
+	return (
+		<SidebarProvider className="flex w-full min-h-screen">
+			<AdminSidebar />
+			<main className="flex flex-col flex-1 w-full min-h-screen">
+				<Outlet />
+			</main>
+		</SidebarProvider>
+	);
 }
