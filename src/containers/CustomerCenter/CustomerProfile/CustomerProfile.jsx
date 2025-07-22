02665f415f0011a0d6380d223070e48c35614b5e
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-07-22 19:22:19 +0700

    refactor: update shipping address card

--- src/containers/CustomerCenter/CustomerProfile/CustomerProfile.jsx
+++ src/containers/CustomerCenter/CustomerProfile/CustomerProfile.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ProfileInfoCard from './ProfileInfoCard'
-import ShippingAddress from './ShippingAddress'
+import React from "react";
+import ProfileInfoCard from "./ProfileInfoCard";
+import ShippingAddress from "./ShippingAddress";
 
 const CustomerProfile = () => {
-  return (
-    <div className="max-w-7xl mx-auto space-y-6">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-          <ProfileInfoCard />
-          <ShippingAddress />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="max-w-7xl mx-auto space-y-6">
+			<div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+				<ProfileInfoCard />
+				<ShippingAddress />
+			</div>
+		</div>
+	);
+};
 
-export default CustomerProfile
+export default CustomerProfile;
